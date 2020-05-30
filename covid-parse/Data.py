dc1fb2b48414ccbe524271cb72e157c949e440ca
ibhatt <ibhatt@umich.edu>
2020-05-30 15:16:56 -0400

    having problems opening CSV because of filepath

--- covid-parse/Data.py
+++ covid-parse/Data.py
@@ -1,16 +1,23 @@
 import csv
+import numpy as np
+import os
+from numpy import array
 class Data:
 # constructor
-    def __init__(self, filename): # add other parameters
-        with open (filename) as csv_file:
+    def __init__(self, filepath, filename): # add other parameters
+        projectPath = os.path.dirname(filepath)
+
+        with open (os.path.join("/mnt/c/", filepath, filename)) as csv_file:
             self.reader = csv.reader(csv_file, delimiter = ',')
             line = 0
+            rowData = np.array([])
             for row in self.reader:
                 if(line != 0):
-                    if row[6] == "Florida" or row[6] == "South Carolina" or row[6] == "Alabama" or row[6] == "Georgia"
-                        self.theData[row[10]] = 
+                    if row[6] == "Florida" or row[6] == "South Carolina" or row[6] == "Alabama" or row[6] == "Georgia":
+                        for x in range (11, 139):
+                            np.append(rowData, row[x])
 
-
+                    line = line + 1
 
     
 
--- covid-parse/covid-parsing.py
+++ covid-parse/covid-parsing.py
@@ -1,9 +1,10 @@
-from covid-parse import Data
+from Data import Data
 
 def main():
     print("Parsing time_series_covid_confirmed_US.csv into dictionary...")
     filename = "time_series_covid_confirmed_US.csv"
-    covid_parsed = Data(filename)
+    filepath = input('Enter filepath to project: ')
+    covid_parsed = Data(filepath,filename)
 
 if __name__ == "__main__":
     main()
