72080412f2ee474f7a93429b0ea53191e3ee42eb
cphi <cphi@umich.edu>
2020-05-30 19:06:24 -0400

    parse

--- covid-parse/Data.py
+++ covid-parse/Data.py
@@ -1,36 +1,35 @@
 import csv
-import numpy as np
 import os
-from numpy import array
+
 class Data:
 # constructor
     def __init__(self, filename): # add other parameters
         self.theData = {}
-        with open (os.path.join("/Users/catherinephilpott/Catherine's Folder/Michigan/nasa-hackathon-2020/covid-parse", filename)) as csv_file:
+        with open (os.path.join("/Users/catherinephilpott/Catherines Folder/Michigan/nasa-hackathon-2020/covid-parse", filename)) as csv_file:
             self.reader = csv.reader(csv_file, delimiter = ',')
             line = 0
-            rowData = []
+            
             for row in self.reader:
-                
+                rowData = []
                 if line != 0:
                     
                     if row[6] == "Florida" or row[6] == "South Carolina" or row[6] == "Alabama" or row[6] == "Georgia":
                         #print(row[6])
-                        for x in range (11, 139):
+                        for x in range (11, 140):
                             
                             rowData.append(row[x])
                             
-                        
-                        
+                            
+                    
                     self.theData[row[10]] = rowData
                 
                 line = line + 1
     def writeRows(self):
-        file = open("/Users/catherinephilpott/Catherine's Folder/Michigan/nasa-hackathon-2020/covid-parse/writeData.txt","r+")
+        file = open("/Users/catherinephilpott/Catherines Folder/Michigan/nasa-hackathon-2020/covid-parse/writeData.txt","r+")
         vector_orange =self.theData["Orange, Florida, US"]
         for x in vector_orange:
             file.write(x)
-            file.write(", ")
+            file.write(",")
         #print(vector_orange)
         #print(self.theData["Madison, Florida, US"])
 
