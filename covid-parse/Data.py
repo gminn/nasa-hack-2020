83ff5ffb83aa605cdaf2b46d6920d7ee684182da
cphi <cphi@umich.edu>
2020-05-31 11:02:13 -0400

    lat/long parse

--- covid-parse/Data.py
+++ covid-parse/Data.py
@@ -1,11 +1,17 @@
 import csv
 import os
+from collections import namedtuple
+
+LatLon = namedtuple("LatLon", "latitude longitude")
 
 class Data:
 # constructor
     def __init__(self, filename): # add other parameters
         self.theData = {}
+        self.countyLatLon = {}
+
         with open (os.path.join("/Users/catherinephilpott/Catherines Folder/Michigan/nasa-hackathon-2020/covid-parse", filename)) as csv_file:
+            print(os.path.join("/Users/catherinephilpott/Catherines Folder/Michigan/nasa-hackathon-2020/covid-parse", filename))
             self.reader = csv.reader(csv_file, delimiter = ',')
             line = 0
             
@@ -19,11 +25,19 @@ class Data:
                             
                             rowData.append(row[x])
                             
-                            
+                        thisCountyLatLong = LatLon(latitude=row[8], longitude=row[9])
+                        self.countyLatLon[row[10]] = thisCountyLatLong
                     
-                    self.theData[row[10]] = rowData
+                        self.theData[row[10]] = rowData
                 
                 line = line + 1
+
+    def getCovidDate(self, countyName)
+        return self.theData[countyname]
+
+    def getCountyLatLon(self, countyName)
+        return self.countyLatLon[countyName]
+
     def writeRows(self):
         file = open("/Users/catherinephilpott/Catherines Folder/Michigan/nasa-hackathon-2020/covid-parse/writeData.txt","r+")
         vector_orange =self.theData["Orange, Florida, US"]
